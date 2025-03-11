f7c6c0c3e1fbdcfd02810a042d996efb5ded0682
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-03-10 22:59:39 -0600

    script files started - vit still needs update
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

---
+++ Vision-Transformer/script.py
@@ -0,0 +1,8 @@
+import torch
+from torch import nn
+from typing import Union, List, Tuple, Optional, Dict
+
+from einops import rearrange
+from einops.layers.torch import Rearrange
+
+from dataclasses import dataclass
