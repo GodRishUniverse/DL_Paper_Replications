78348b65239a85153fc05f9365faf352e0154452
Rishabh Agarwal <agarwalrishabh2005@gmail.com>
2025-06-29 22:51:18 -0600

    more boilerplate for mamba - we are just getting started
    Signed-off-by: Rishabh Agarwal <agarwalrishabh2005@gmail.com>

--- paperreplications/Mamba/script.py
+++ paperreplications/Mamba/script.py
@@ -1,7 +1,10 @@
+import math
 import torch
 import torch.nn as nn
 
 from typing import List, Optional, Tuple, Dict, Union
+
+from einops import rearrange, repeat
 
 from dataclasses import dataclass
 
@@ -11,18 +14,45 @@ import torch.nn.functional as F
 @dataclass
 class MambaConfig:
     d_model: int
-    step_size: int
+    projection_size: int
+
+    conv_ker: int
+    conv_bias: bool = True
+    bias: bool = False  
+    device: str = "cuda"
+    dtype: str = None
 
 # block used in the mamba model
 class MambaBlock(nn.Module):
     def __init__(self, args: MambaConfig):
+
+        kwargs = {"device" : args.device, "dtype" : args.dtype} # reduced code repetition here as same keyword arguments added
+        self.args = args
+      
+        self.activation = nn.SiLU()
+
+        self.proj_1 = nn.Linear(in_features = args.d_model, out_features = args.d_model*2, bias= args.bias, **kwargs )
+        self.proj_2 = nn.Linear(in_features = args.d_model, out_features = args.d_model*2, bias = args.bias, **kwargs)
+
+        self.conv_size = int(args.d_model * args.projection_size)
+        self.conv = nn.Conv1d(in_channels = self.conv_size, out_channels= self.conv_size, kernel_size= args.conv_ker, groups=self.conv_size-1, bias=args.conv_bias,  **kwargs)
+        
+
         ...
-    def forward(self, ):
+    def forward(self, hidden_states ):
         ...
-    def ssm(self, ):
+    def ssm(self, x: torch.Tensor, y: torch.Tensor ):
+        """
+            x: [B,L,D]
+            y: [B,L,D]
+        """
+       
         ...
-    def selective_scan(self, ):
+    def selective_scan(self, x: torch.Tensor, y: torch.Tensor):
+        """
+            x: [B,L,D]
+            y: [B,L,D]
+        """
         ...
     
-    def ssm_step():
-        ...
+
